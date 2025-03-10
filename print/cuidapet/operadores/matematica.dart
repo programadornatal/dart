ce1b8b324f2fbe9b53c539882862f370e8200b33
Fabio Santos <programadornatal@gmail.com>
2025-03-10 14:25:51 -0300

    Add math operators

---
+++ print/cuidapet/operadores/matematica.dart
@@ -0,0 +1,27 @@
+void main() {
+    // Adição +
+    // Subtração -
+    // Multiplicação *
+    // Divisão /
+    // Resto da divisão %
+    // Precedencia ()
+
+    // Numeros int, double
+
+    int numero1 = 10;
+    int numero2 = 3;
+
+    int adicao = numero1 + numero2;
+    int sub = numero1 - numero2;
+    int multi = numero1 * numero2;
+    double quo = numero1 / numero2;
+    int resto = numero1 % numero2;
+    double precedencia = numero1 / (numero1 + numero2);
+
+    print(adicao);
+    print(sub);
+    print(multi);
+    print(quo);
+    print(resto);
+    print(precedencia);
+}
